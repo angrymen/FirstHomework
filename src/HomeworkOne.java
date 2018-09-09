9104dc6afb681f11fdc06b8721623950d0be7ed5
Michal Madejski <michalmadejski00@gmail.com>
2018-09-09 11:30:40 +0200

    Trzeci commit: dodano rozwiazanie version 1.1

--- src/HomeworkOne.java
+++ src/HomeworkOne.java
@@ -1,9 +1,12 @@
 public class HomeworkOne {
     public static void main(String[] args) {
+        // version 1.0
         System.out.println("Uczę");
         System.out.println("się");
         System.out.println("programowania");
         System.out.println("i");
         System.out.println("commitowania");
+        // version 1.1
+        System.out.println("Uczę\nsię\nprogramowania\ni\ncommitowania");
     }
 }
